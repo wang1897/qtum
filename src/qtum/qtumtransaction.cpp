147af29954efde76b913f1110b556ca6c1c5d065
Huaming He <hhmingw@yahoo.com>
2018-04-26 12:55:52 +0800

    update format

--- src/qtum/qtumtransaction.cpp
+++ src/qtum/qtumtransaction.cpp
@@ -2,9 +2,8 @@
 #include "qtumtransaction.h"
 
 static const uint8_t byteCodePre[]={'b','y','t','e','c','o','d','e','_'};
-bool DeltaDB:: writeByteCode(UniversalAddress address, valtype byteCode)
-{
 
+bool DeltaDB:: writeByteCode(UniversalAddress address, valtype byteCode){
   if(address.version == AddressVersion::X86){
 	std::vector<uint8_t> K(byteCodePre, byteCodePre + sizeof(byteCodePre)/sizeof(uint8_t));  
 	K.insert(K.end(), address.data.begin(), address.data.end());  
@@ -13,14 +12,13 @@ bool DeltaDB:: writeByteCode(UniversalAddress address, valtype byteCode)
   return false;
 }
 
-bool DeltaDB:: readByteCode(UniversalAddress address, valtype& byteCode)
-{
- if(address.version == AddressVersion::X86){
-   std::vector<uint8_t> K(byteCodePre, byteCodePre + sizeof(byteCodePre)/sizeof(uint8_t));	
-   K.insert(K.end(), address.data.begin(), address.data.end());
-   return Read(K, byteCode);   
- }
- return false;
+bool DeltaDB:: readByteCode(UniversalAddress address, valtype& byteCode){
+  if(address.version == AddressVersion::X86){
+    std::vector<uint8_t> K(byteCodePre, byteCodePre + sizeof(byteCodePre)/sizeof(uint8_t));	
+    K.insert(K.end(), address.data.begin(), address.data.end());
+    return Read(K, byteCode);   
+  }
+  return false;
 }
 
 
